c743ad57f79fe960759e2bf94f111dc3b241afa1
Alexian312 <alexian.jaccard@eduvaud.ch>
2024-12-05 12:13:17 +0100

    regle

--- dame_main.py
+++ dame_main.py
@@ -6,44 +6,39 @@ Version : 3.7
 Purpose : Jeu de dames avec la librairie pygame
 """
 
-import dame_gfx
-import dame_regle
-
 import pygame
 
-def dessine_case(col, row, couleur):
-    """Dessine une case du damier à la position (col, row) avec la couleur spécifiée"""
+
+def dessine_case(col, row):
+    """Dessine une case du damier à la position (col, row)"""
+    global screen, case_size, cases_blanches, cases_noires, marge_gauche, marge_haut
+
+    # Alterne entre case blanche et noire
+    couleur = cases_blanches if (col + row) % 2 else cases_noires
+
+    # Dessine la case
     pygame.draw.rect(screen, couleur,
                      (marge_gauche + col * case_size,
                       marge_haut + row * case_size,
                       case_size,
                       case_size), 0)
 
-def dessine_toutes_les_cases():
-    """Redessine toutes les cases du plateau"""
-    for row in range(nb_lignes):
-        for col in range(nb_colonnes):
-            couleur = cases_blanches if (col + row) % 2 else cases_noires
-            dessine_case(col, row, couleur)
 
-def bouger_pion(pion, pion_ligne, pion_pos, nouvelle_ligne, nouvelle_pos):
-    """Déplace le pion du damier, met à jour l'affichage"""
-    # Efface l'ancienne position
-    couleur_case = cases_blanches if (pion_pos + pion_ligne) % 2 else cases_noires
-    dessine_case(pion_pos, pion_ligne, couleur_case)
+def afficher_plateau(plateau):
+    """Affiche le plateau dans la console"""
+    for ligne in plateau:
+        print(" ".join(str(case) for case in ligne))
 
-    # Met à jour la nouvelle position
-    pion_ligne = nouvelle_ligne
-    pion_pos = nouvelle_pos
-    # Affiche le pion à sa nouvelle position
-    screen.blit(pion, (marge_gauche + case_size * pion_pos, marge_haut + case_size * pion_ligne))
 
-    return pion_ligne, pion_pos
+
 
 # ------------ MAIN -------------
+
+# Paramètres du plateau
 case_size = 80
-cases_blanches = (240, 230, 140)
-cases_noires = (160, 82, 45)
+cases_blanches = (255, 255, 255)
+cases_noires = (0, 0, 0)
+
 
 # Marges autour du damier
 marge_gauche = 10
@@ -61,7 +56,7 @@ nb_colonnes = 10  # Nombre de colonnes
 # Taille de la fenêtre
 window_size = (case_size * nb_colonnes + marge_gauche + marge_droite,
                case_size * nb_lignes + marge_haut + marge_bas)
-window_color = (255, 255, 255)
+window_color = (250, 202, 255)
 
 # Création de la fenêtre
 screen = pygame.display.set_mode(window_size)
@@ -74,22 +69,21 @@ icon = pygame.image.load(path_to_images + "International_draughts.png")
 pygame.display.set_icon(icon)
 
 # Dessin du damier
-dessine_toutes_les_cases()
+for row in range(nb_lignes):
+    for col in range(nb_colonnes):
+        dessine_case(col, row)
 
 # Affichage du pion initial
-pion_pos = 0  # Position initiale du pion blanc
-pion_ligne = 0
+pion_pos = 0  # Position initiale du pion
 pion = pygame.image.load(path_to_images + "MA-24_pion.png")
 pion = pygame.transform.scale(pion, (case_size, case_size))
 screen.blit(pion, (marge_gauche, marge_haut))
 
-# Affichage du pion noir
-pion_pos_noir = 0  # Position initiale du pion noir
-pion_ligne_noir = 9  # Commence en bas (ligne 9)
+#afiché le deuxième pion
+pion_pos_noir = 0  # Position initiale du pion
 pion_noir = pygame.image.load(path_to_images + "MA-24_pion_noir.png")
 pion_noir = pygame.transform.scale(pion_noir, (case_size, case_size))
-screen.blit(pion_noir, (marge_gauche, marge_haut + case_size * pion_ligne_noir))
-
+screen.blit(pion_noir, (marge_gauche, marge_haut))
 pygame.display.flip()
 
 # Boucle principale
@@ -100,30 +94,16 @@ while running:
             running = False
 
         btn_presse = pygame.key.get_pressed()
+        if btn_presse[pygame.K_RIGHT]:
+            bouge_droite()
+        elif btn_presse[pygame.K_LEFT]:
+            bouge_gauche()
+        elif btn_presse[pygame.K_UP]:
+            bouge_haut()
+        elif btn_presse[pygame.K_DOWN]:
+            bouge_bas()
+        elif btn_presse[pygame.K_q]:
+            running = False
 
-        # Déplacer le pion blanc vers le bas à droite (1 case)
-        #if btn_presse[pygame.K_d]:
-            #if pion_pos < (nb_colonnes - 1) and pion_ligne < (nb_lignes - 1):
-                #pion_ligne, pion_pos = bouger_pion(pion, pion_ligne, pion_pos, pion_ligne + 1, pion_pos + 1)
-
-        # Déplacer le pion blanc vers le bas à gauche (1 case)
-        #elif btn_presse[pygame.K_a]:
-            #if pion_pos > 0 and pion_ligne < (nb_lignes - 1):
-                #pion_ligne, pion_pos = bouger_pion(pion, pion_ligne, pion_pos, pion_ligne + 1, pion_pos - 1)
-
-        # Déplacer le pion noir vers le haut à gauche (1 case)
-        #elif btn_presse[pygame.K_w]:
-            #if pion_pos_noir > 0 and pion_ligne_noir > 0:
-                #pion_ligne_noir, pion_pos_noir = bouger_pion(pion_noir, pion_ligne_noir, pion_pos_noir, pion_ligne_noir - 1, pion_pos_noir - 1)
-
-        # Déplacer le pion noir vers le haut à droite (1 case)
-        #elif btn_presse[pygame.K_s]:
-           #if pion_pos_noir < (nb_colonnes - 1) and pion_ligne_noir > 0:
-                #pion_ligne_noir, pion_pos_noir = bouger_pion(pion_noir, pion_ligne_noir, pion_pos_noir, pion_ligne_noir - 1, pion_pos_noir + 1)
-
-        #elif btn_presse[pygame.K_q]:
-            #running = False
-
-        #pygame.display.update()
-
+        pygame.display.update()
 pygame.quit()
--- dame_regle.py
+++ dame_regle.py
@@ -17,6 +17,55 @@ def bouger_pion(pion, pion_ligne, pion_pos, nouvelle_ligne, nouvelle_pos):
 
     return pion_ligne, pion_pos
 
+def deplacer_pion(plateau, x1, y1, x2, y2):
+    """
+    Déplace un pion du plateau de (x1, y1) à (x2, y2).
+    Vérifie si la destination est vide (0).
+    """
+    if plateau[x2][y2] == 0:  # Si la case de destination est vide
+        plateau[x2][y2] = plateau[x1][y1]  # Déplace le pion
+        plateau[x1][y1] = 0  # Vide la case de départ
+        return True
+    else:
+        print("La case de destination est occupée!")
+        return False
+
+
+def bouge_droite():
+    """Déplace le pion vers la droite"""
+    global screen, case_size, marge_gauche, marge_haut, pion_pos, pion, nb_colonnes
+    if pion_pos < (nb_colonnes - 1):
+        dessine_case(pion_pos, 0)
+        pion_pos += 1
+    screen.blit(pion, (marge_gauche + case_size * pion_pos, marge_haut))
+
+
+def bouge_gauche():
+    """Déplace le pion vers la gauche"""
+    global screen, case_size, marge_gauche, marge_haut, pion_pos, pion, nb_colonnes
+    if pion_pos > 0:
+        dessine_case(pion_pos, 0)
+        pion_pos -= 1
+    screen.blit(pion, (marge_gauche + case_size * pion_pos, marge_haut))
+
+def bouge_haut():
+    """Déplace le pion noir vers le haut"""
+    global screen, case_size, marge_gauche, marge_haut, pion_pos_noir, pion_noir, nb_colonnes
+    if pion_pos_noir > 0:
+        dessine_case(0, pion_pos_noir)
+        pion_pos_noir -= 1
+    screen.blit(pion_noir, (marge_gauche, marge_haut + case_size * pion_pos_noir))
+
+
+def bouge_bas():
+    """Déplace le pion noir vers le bas"""
+    global screen, case_size, marge_gauche, marge_haut, pion_pos_noir, pion_noir, nb_colonnes
+    if pion_pos_noir < (nb_colonnes - 1):
+        dessine_case(0, pion_pos_noir)
+        pion_pos_noir += 1
+    screen.blit(pion_noir, (marge_gauche, marge_haut + case_size * pion_pos_noir))
+
+
 
 # Boucle principale
 running = True
